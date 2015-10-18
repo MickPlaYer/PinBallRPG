411e4a10224f1961dc8d7d2057517c432c183d67
kevin <kevin15121@gmail.com>
2015-10-18 19:24:08 +0800

    add level select slider

--- Assets/MainScene/script/GameStart.cs
+++ Assets/MainScene/script/GameStart.cs
@@ -1,25 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using SimpleJSON;
+using UnityEngine.UI;
 
 public class GameStart : MonoBehaviour {
+    public Slider _levelSelect;
+
     public HeroData3 _heroData;
     float _bAtk=0, _bHp=0, _bRe=0;
     string hero;
     // Use this for initialization
     void Start () {
-
+        _levelSelect.maxValue = _heroData.getMaxLevel();
          hero = PlayerPrefs.GetString("battle_value", _heroData.getData());
+       
         setValue();
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+    }
 
     public void setValue()
     {
+        PlayerPrefs.SetInt("current_level", (int)_levelSelect.value);
         _heroData.setValue();    
     }
 
---
+++ Assets/TextWithSlider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextWithSlider : MonoBehaviour {
+    public Text _level;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        _level.text = GetComponent<Slider>().value.ToString();
+    }
+}
