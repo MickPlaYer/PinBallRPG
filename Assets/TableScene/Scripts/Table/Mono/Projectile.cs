96ca56d5ca69e723c9816838feda5b6ac17624dd
mickplayer <mickgamehome@gmail.com>
2015-10-19 23:54:46 +0800

    Add two skill. Fix Boss presentation.

---
+++ Assets/TableScene/Scripts/Table/Mono/Projectile.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    private GameObject _target;
+    private int _damage;
+    private Vector2 _direction;
+    public Rigidbody2D _rigidbody;
+    public float angularVelocity;
+    public PinBallTable _table;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody.angularVelocity = 500;
+        _rigidbody.velocity = _direction *= 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Make attraction
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag != "Wall")
+        {
+            if (other.gameObject == _target)
+            {
+                HurtTarget();
+            }
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Set the target.
+    public void SetTarget(GameObject target)
+    {
+        _target = target;
+        _direction = target.transform.position - transform.position;
+        _direction.Normalize();
+    }
+
+    public int Damage
+    {
+        get { return _damage; }
+        set { _damage = value; }
+    }
+
+    // Hurt target.
+    private void HurtTarget()
+    {
+        if (_target.name == "Ball")
+        {
+            _table.HurtHero(_damage);
+        }
+        else if (_target.name == "Boss")
+        {
+            _table.HurtBoss(_damage);
+        }
+        gameObject.SetActive(false);
+    }
+}
